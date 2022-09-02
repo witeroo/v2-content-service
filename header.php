d0ab54636d25ba47d6f1fae91f3066d82c64f808
itolu <i_tolu@yahoo.com>
2022-09-02 23:15:31 +0100

    :sparkles: Feat: facebook share with social share preview

--- header.php
+++ header.php
@@ -7,6 +7,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
     <title>Witeroo <?php wp_title('-',1,'left'); ?></title>
+
+    <meta property="og:url" content="<?php echo home_url( $wp->request ) ?>" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="<?php echo get_the_title(); ?>" />
+    <meta property="og:description" content="<?php echo get_the_excerpt(); ?>" />
+    <meta property="og:image" content="<?php echo get_the_post_thumbnail_url(); ?>" />
+
     <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/favicon-16x16.png">
